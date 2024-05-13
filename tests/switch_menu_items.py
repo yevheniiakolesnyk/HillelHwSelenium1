61ce031ee3dd093d665baac5e73dbaf0495ec685
Yevheniia Kolesnyk <145061254+yevheniiakolesnyk@users.noreply.github.com>
2024-05-13 15:35:13 +0300

    Update switch_menu_items.py

--- tests/switch_menu_items.py
+++ tests/switch_menu_items.py
@@ -13,7 +13,6 @@ def driver():
     driver.get(BASE_URL)
     driver.implicitly_wait(10)
     yield driver
-    time.sleep(2)
     driver.quit()
 
 
@@ -21,9 +20,7 @@ def test_do(driver):
     wait = WebDriverWait(driver,10)
     top_menu_item_1 = wait.until(EC.element_to_be_clickable((By.ID, 'ui-id-3')))
     top_menu_item_1.click()
-    time.sleep(2)
     top_menu_item_2 = driver.find_element(By.ID, 'ui-id-4')
     top_menu_item_2.click()
-    time.sleep(2)
     top_menu_item_3 = driver.find_element(By.ID, 'ui-id-5')
     top_menu_item_3.click()
